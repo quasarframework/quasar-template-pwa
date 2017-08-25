0581b533c680705eb9214bcf771ccf1eda6e9a9e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-08-25 18:55:50 +0300

    feat: Updates for basic content

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
-    <router-view />
+    <header></header>
+    <main>
+      <img src="~assets/quasar-logo-full.svg" alt="Quasar PWA">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
@@ -12,4 +16,15 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="stylus">
+@import '~variables'
+
+main
+  text-align center
+  margin-top 40px
+
+header
+  margin 0
+  height 10px
+  background-color $primary
+</style>
